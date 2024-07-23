59e2b8bda5ebcb66915e5a74b4ee2ab946d7860a
Valentin Perignon <valentin.perignon@infomaniak.com>
2024-07-23 13:16:43 +0200

    feat: Create Dynamic Type Size fonts

--- SwissTransferCoreUI/Guide/Font+Extension.swift
+++ SwissTransferCoreUI/Guide/Font+Extension.swift
@@ -16,21 +16,88 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import Foundation
 import SwiftUI
 
 public extension Font {
-    static let header1 = system(size: 22).weight(.semibold)
-    static let header2 = system(size: 18).weight(.semibold)
-    static let bodyRegular = system(size: 16).weight(.regular)
-    static let bodyMedium = system(size: 16).weight(.medium)
-    static let bodySmallRegular = system(size: 14).weight(.regular)
-    static let bodySmallMedium = system(size: 14).weight(.medium)
-    static let labelRegular = system(size: 12).weight(.regular)
-    static let labelMedium = system(size: 12).weight(.medium)
+    /// List of fonts used by the SwissTransfer app.
+    enum ST {
+        // MARK: - Base
 
-    // MARK: - Specific Font
+        /// Figma name: Titre H1
+        public static let title = Font.dynamicTypeSizeFont(size: 22, weight: .semibold, relativeTo: .title)
+        /// Figma name: Titre H2
+        public static let title2 = Font.dynamicTypeSizeFont(size: 18, weight: .semibold, relativeTo: .title2)
+        /// Figma name: Body Medium
+        public static let headline = Font.dynamicTypeSizeFont(size: 16, weight: .medium, relativeTo: .headline)
+        /// Figma name: Body Regular
+        public static let body = Font.dynamicTypeSizeFont(size: 16, weight: .regular, relativeTo: .body)
+        /// Figma name: Body Small Regular
+        public static let callout = Font.dynamicTypeSizeFont(size: 14, weight: .regular, relativeTo: .callout)
+        /// Figma name: Body Small Medium
+        public static let calloutMedium = Font.dynamicTypeSizeFont(size: 14, weight: .medium, relativeTo: .callout)
+        /// Figma name: Label Regular
+        public static let caption = Font.dynamicTypeSizeFont(size: 12, weight: .regular, relativeTo: .caption)
+        /// Figma name: Label Medium
+        public static let captionMedium = Font.dynamicTypeSizeFont(size: 12, weight: .medium, relativeTo: .caption)
 
-    static let specificMedium = system(size: 22).weight(.medium)
-    static let specificLight = system(size: 22).weight(.light)
+        // MARK: - Specific Font
+
+        /// Figma name: Spécifique 32 Medium
+        static let specificLargeTitleMedium = Font.dynamicTypeSizeFont(size: 32, weight: .medium, relativeTo: .largeTitle)
+        /// Figma name: Spécifique 22 Medium
+        static let specificTitleMedium = Font.dynamicTypeSizeFont(size: 22, weight: .medium, relativeTo: .title)
+        /// Figma name: Spécifique 22 Light
+        static let specificTitleLight = Font.dynamicTypeSizeFont(size: 22, weight: .light, relativeTo: .title)
+        /// Figma name: Spécifique 18 Light
+        static let specificTitle2Light = Font.dynamicTypeSizeFont(size: 18, weight: .light, relativeTo: .title2)
+    }
+
+    /// Create a custom font with the UIFont preferred font family
+    ///
+    /// SwiftUI will use the default system font with the specified weight and size use Dynamic Type Size.
+    private static func dynamicTypeSizeFont(size: CGFloat, weight: Weight, relativeTo textStyle: TextStyle) -> Font {
+        let fontFamily = UIFont.preferredFont(forTextStyle: .body).familyName
+        return custom(fontFamily, size: size, relativeTo: textStyle).weight(weight)
+    }
+}
+
+#Preview {
+    VStack(alignment: .leading, spacing: 12) {
+        Text("BASE")
+            .font(.title3)
+            .foregroundStyle(Color.gray)
+
+        Text("Title")
+            .font(.ST.title)
+        Text("Title 2")
+            .font(.ST.title2)
+        Text("Headline")
+            .font(.ST.headline)
+        Text("Body")
+            .font(.ST.body)
+        Text("Callout")
+            .font(.ST.callout)
+        Text("Callout Medium")
+            .font(.ST.calloutMedium)
+        Text("Caption")
+            .font(.ST.caption)
+        Text("Caption Medium")
+            .font(.ST.captionMedium)
+
+        Divider()
+
+        Text("SPECIFIC")
+            .font(.title3)
+            .foregroundStyle(Color.gray)
+
+        Text("Specific Large Title Medium")
+            .font(.ST.specificLargeTitleMedium)
+        Text("Specific Title Medium")
+            .font(.ST.specificTitleMedium)
+        Text("Specific Title Light")
+            .font(.ST.specificTitleLight)
+        Text("Specific Title 2 Light")
+            .font(.ST.specificTitle2Light)
+    }
+    .padding()
 }
