1ba60fd19d1fae477c820982f04e60ecf2b1bb1b
Ambroise Decouttere <ambroise.decouttere@infomaniak.com>
2024-09-24 11:05:41 +0200

    fix: Remove Macro and use @Entry

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -16,7 +16,6 @@ let package = Package(
     name: "SwissTransfer",
     dependencies: [
         .package(url: "https://github.com/Infomaniak/ios-core-ui", .upToNextMajor(from: "11.1.0")),
-        .package(url: "https://github.com/Infomaniak/multiplatform-SwissTransfer", .upToNextMajor(from: "0.0.1")),
-        .package(url: "https://github.com/Wouter01/SwiftUI-Macros.git", .upToNextMajor(from: "1.0.0"))
+        .package(url: "https://github.com/Infomaniak/multiplatform-SwissTransfer", .upToNextMajor(from: "0.0.1"))
     ]
 )
