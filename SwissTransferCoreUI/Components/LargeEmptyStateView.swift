0fa616d3fbf771e7e818b84d26b49a72d402a4db
Valentin Perignon <valentin.perignon@infomaniak.com>
2024-10-17 15:22:50 +0200

    feat: Add LargeEmptyState component

--- SwissTransferCoreUI/Components/LargeEmptyStateView.swift
+++ SwissTransferCoreUI/Components/LargeEmptyStateView.swift
@@ -1,4 +1,3 @@
-//
 /*
  Infomaniak SwissTransfer - iOS App
  Copyright (C) 2024 Infomaniak Network SA
@@ -17,14 +16,51 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import InfomaniakCoreSwiftUI
+import STResources
 import SwiftUI
 
-struct LargeEmptyStateView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public struct LargeEmptyStateView: View {
+    let image: Image
+    let title: String
+    let subtitle: String
+    let imageHorizontalPadding: CGFloat?
+
+    public init(image: Image, title: String, subtitle: String, imageHorizontalPadding: CGFloat? = IKPadding.medium) {
+        self.image = image
+        self.title = title
+        self.subtitle = subtitle
+        self.imageHorizontalPadding = imageHorizontalPadding
+    }
+
+    public var body: some View {
+        VStack(spacing: 32) {
+            image
+                .resizable()
+                .scaledToFit()
+                .padding(.horizontal, imageHorizontalPadding ?? 0)
+                .frame(maxWidth: 400)
+
+            VStack(spacing: IKPadding.medium) {
+                Text(title)
+                    .font(.ST.title)
+                    .foregroundStyle(Color.ST.textPrimary)
+
+                Text(subtitle)
+                    .font(.ST.body)
+                    .foregroundStyle(Color.ST.textSecondary)
+                    .frame(maxWidth: 300)
+            }
+        }
+        .multilineTextAlignment(.center)
     }
 }
 
 #Preview {
-    LargeEmptyStateView()
+    LargeEmptyStateView(
+        image: STResourcesAsset.Images.beersHands.swiftUIImage,
+        title: "Empty State Title",
+        subtitle: "Consequat magna cupidatat aute fugiat quis dolore ea labore nisi velit. Culpa deserunt adipisicing velit consequat.",
+        imageHorizontalPadding: 0
+    )
 }
