fa6a03e2bdd8d276d3a14982ae6749ad2ddebac6
Ambroise Decouttere <ambroise.decouttere@infomaniak.com>
2024-10-23 11:15:56 +0200

    feat: File size added as URL extension

--- SwissTransferCore/Utils/URL+Extension.swift
+++ SwissTransferCore/Utils/URL+Extension.swift
@@ -56,4 +56,28 @@ public extension URL {
         try FileManager.default.createDirectory(at: tmpDirectory, withIntermediateDirectories: true)
         return tmpDirectory
     }
+
+    /// Size of the file/folder in bytes
+    func size() -> Int {
+        guard let resources = try? resourceValues(forKeys: [.isDirectoryKey, .fileSizeKey]) else { return 0 }
+
+        if resources.isDirectory == true {
+            return folderSize() ?? 0
+        } else {
+            return resources.fileSize ?? 0
+        }
+    }
+
+    private func folderSize() -> Int? {
+        guard let enumerator = FileManager.default.enumerator(at: self, includingPropertiesForKeys: [.fileSizeKey])
+        else { return nil }
+        var size = 0
+        for case let fileURL as URL in enumerator {
+            guard let fileSize = try? fileURL.resourceValues(forKeys: [.fileSizeKey]).fileSize else {
+                continue
+            }
+            size += fileSize
+        }
+        return size
+    }
 }
