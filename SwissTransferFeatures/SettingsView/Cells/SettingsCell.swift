ca41b81bfdfc56a5eeb8b13652727b5a08d8a4b7
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2024-11-13 08:34:51 +0100

    chore: PR Feedback

--- SwissTransferFeatures/SettingsView/Cells/SettingsCell.swift
+++ SwissTransferFeatures/SettingsView/Cells/SettingsCell.swift
@@ -65,7 +65,7 @@ struct SingleLabelSettingsCell: View {
 
             if let rightIconAsset {
                 Image(asset: rightIconAsset)
-                    .iconSize(.large)
+                    .iconSize(.medium)
             }
         }
     }
