b8bea2e71621895c5de9de078bb58ba6f149f512
Ambroise Decouttere <ambroise.decouttere@infomaniak.com>
2024-12-09 12:55:05 +0100

    fix: Show folder in TransferList

--- SwissTransferFeatures/TransferList/TransferCellThumbnailsView.swift
+++ SwissTransferFeatures/TransferList/TransferCellThumbnailsView.swift
@@ -42,7 +42,11 @@ struct TransferCellThumbnailsView: View {
     var body: some View {
         HStack(spacing: IKPadding.small) {
             ForEach(itemsToShow, id: \.uid) { file in
-                SmallThumbnailView(url: file.localURL, mimeType: file.mimeType ?? "", size: .small)
+                if file.isFolder {
+                    SmallThumbnailView()
+                } else {
+                    SmallThumbnailView(url: file.localURL, mimeType: file.mimeType ?? "")
+                }
             }
             if additionalItemsCount > 0 {
                 SmallMoreItemsThumbnailView(count: additionalItemsCount)
