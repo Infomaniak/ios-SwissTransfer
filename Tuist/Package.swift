923cff1afe94e4f47df25aae33561415778cf33c
Valentin Perignon <valentin.perignon@infomaniak.com>
2024-12-19 13:50:02 +0100

    chore: Bump core-ui to 15.0.0

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     name: "SwissTransfer",
     dependencies: [
         .package(url: "https://github.com/Infomaniak/ios-core", .upToNextMajor(from: "12.7.0")),
-        .package(url: "https://github.com/Infomaniak/ios-core-ui", .upToNextMajor(from: "14.3.0")),
+        .package(url: "https://github.com/Infomaniak/ios-core-ui", .upToNextMajor(from: "15.0.0")),
         .package(url: "https://github.com/Infomaniak/ios-onboarding", .upToNextMajor(from: "1.1.2")),
         .package(url: "https://github.com/Infomaniak/ios-device-check", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/Infomaniak/multiplatform-SwissTransfer", .upToNextMajor(from: "0.1.0")),
