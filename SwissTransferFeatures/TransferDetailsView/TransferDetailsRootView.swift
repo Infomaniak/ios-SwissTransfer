fca391a47cbfdf0b6e124f2a36ec3e0250058f3e
Valentin Perignon <valentin.perignon@infomaniak.com>
2025-01-20 08:12:01 +0100

    chore: Move files to groups

---
+++ SwissTransferFeatures/TransferDetailsView/TransferDetailsRootView.swift
@@ -0,0 +1,40 @@
+/*
+ Infomaniak SwissTransfer - iOS App
+ Copyright (C) 2025 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import SwiftUI
+import STCore
+
+public struct TransferState {
+    let transfer: TransferUi?
+    let status: TransferStatus?
+
+    public init(transfer: TransferUi?, status: TransferStatus?) {
+        self.transfer = transfer
+        self.status = status
+    }
+}
+
+struct TransferDetailsRootView: View {
+    let state: TransferState
+
+    var body: some View {
+        ZStack {
+
+        }
+    }
+}
