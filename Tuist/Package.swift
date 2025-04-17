65d42903f952aed223bab6e35e2c768d9b2a7210
Matthieu Déglon <matthieu.deglon@infomaniak.com>
2025-04-17 09:03:09 +0200

    chore: Bump version checker to 10.1.3

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -39,6 +39,6 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-collections", .upToNextMajor(from: "1.1.4")),
         .package(url: "https://github.com/matomo-org/matomo-sdk-ios", .upToNextMajor(from: "7.7.0")),
         .package(url: "https://github.com/airbnb/lottie-spm.git", .upToNextMajor(from: "4.5.1")),
-        .package(url: "https://github.com/Infomaniak/ios-version-checker", .upToNextMajor(from: "10.1.2"))
+        .package(url: "https://github.com/Infomaniak/ios-version-checker", .upToNextMajor(from: "10.1.3"))
     ]
 )
