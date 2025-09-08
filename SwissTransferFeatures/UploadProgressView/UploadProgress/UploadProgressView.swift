8784e665434c6f217c617c2a9cd8ec6d0490497d
Valentin Perignon <valentin.perignon@infomaniak.com>
2025-09-08 13:20:59 +0200

    chore: Do not send to Sentry DailyQuotaExceeded, EmailValidationRequired or DomainBlockedException
    These cases are perfectly handled by the app

--- SwissTransferFeatures/UploadProgressView/UploadProgress/UploadProgressView.swift
+++ SwissTransferFeatures/UploadProgressView/UploadProgress/UploadProgressView.swift
@@ -151,17 +151,13 @@ public struct UploadProgressView: View {
             sendErrorToSentryIfNeeded(error: error.underlyingError)
             rootTransferViewState.transition(to: .error(.appIntegrity))
         } catch let error as NSError where error.kotlinException is STNContainerErrorsException.DailyQuotaExceededException {
-            sendErrorToSentryIfNeeded(error: error)
             rootTransferViewState.transition(to: .error(.dailyQuotaExceeded))
         } catch let error as NSError where error.kotlinException is STNContainerErrorsException.EmailValidationRequired {
             guard let newUploadSession = await viewModel.toNewUploadSessionWith(newTransferFileManager) else {
                 return
             }
-
-            sendErrorToSentryIfNeeded(error: error)
             rootTransferViewState.transition(to: .verifyMail(newUploadSession))
         } catch let error as NSError where error.kotlinException is STNContainerErrorsException.DomainBlockedException {
-            sendErrorToSentryIfNeeded(error: error)
             rootTransferViewState.transition(to: .error(.restrictedLocation))
         } catch {
             guard (error as NSError).code != NSURLErrorCancelled else { return }
