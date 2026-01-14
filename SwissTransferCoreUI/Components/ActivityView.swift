6cbb27c63238614695e74c2b84e0bf224500fc5c
Ambroise Decouttere <ambroise.decouttere@infomaniak.com>
2026-01-14 14:13:58 +0100

    fix: Use ActivityItemSource for sharing

--- SwissTransferCoreUI/Components/ActivityView.swift
+++ SwissTransferCoreUI/Components/ActivityView.swift
@@ -16,21 +16,52 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import LinkPresentation
 import SwiftUI
 
 public struct ActivityView: UIViewControllerRepresentable {
-    let sharedFileURLs: [URL]
+    let activityItems: [FileShareModel]
 
     public init(sharedFileURLs: [URL]) {
-        self.sharedFileURLs = sharedFileURLs
+        activityItems = sharedFileURLs.compactMap { try? FileShareModel(url: $0) }
     }
 
     public func makeUIViewController(context: UIViewControllerRepresentableContext<ActivityView>) -> UIActivityViewController {
-        return UIActivityViewController(activityItems: sharedFileURLs, applicationActivities: nil)
+        return UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
     }
 
     public func updateUIViewController(
         _ uiViewController: UIActivityViewController,
         context: UIViewControllerRepresentableContext<ActivityView>
     ) {}
+
+    class FileShareModel: NSObject, UIActivityItemSource {
+        let url: URL
+        let data: Data
+        let title: String
+
+        init(url: URL) throws {
+            self.url = url
+            title = url.lastPathComponent
+            data = try Data(contentsOf: url)
+            super.init()
+        }
+
+        func activityViewControllerPlaceholderItem(_ activityViewController: UIActivityViewController) -> Any {
+            data
+        }
+
+        func activityViewController(_ activityViewController: UIActivityViewController, itemForActivityType activityType: UIActivity.ActivityType?) -> Any? {
+            data
+        }
+
+        func activityViewControllerLinkMetadata(_ activityViewController: UIActivityViewController) -> LPLinkMetadata? {
+            let metadata = LPLinkMetadata()
+            metadata.title = title
+            metadata.url = url
+            metadata.originalURL = url
+            metadata.iconProvider = NSItemProvider(contentsOf: url)
+            return metadata
+        }
+    }
 }
