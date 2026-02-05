08ca57a593f9b0ba51b8724aec18198962cb45f2
Elena Willen <elena.willen@infomaniak.com>
2026-02-05 14:56:51 +0100

    feat: Create delete account button

--- SwissTransferFeatures/SettingsView/SettingsView.swift
+++ SwissTransferFeatures/SettingsView/SettingsView.swift
@@ -118,6 +118,12 @@ public struct SettingsView: View {
                                             leadingIcon: STResourcesAsset.Images.shield)
                 }
                 .settingsCell()
+                
+                Link(destination: URL(fileURLWithPath: "")) { // TODO: Handle delete account
+                    SingleLabelSettingsCell(title: STResourcesStrings.Localizable.settingsDeleteMyAccount, leadingIcon: STResourcesAsset.Images.delete,
+                                            trailingIcon: STResourcesAsset.Images.export)
+                }
+                .settingsCell()
             }
         }
         .listStyle(.insetGrouped)
