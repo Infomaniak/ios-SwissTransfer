c2b7fb85500f39cb494e875a506aa7e9bed761c1
Killian Mathias <killian.mathias@infomaniak.com>
2026-04-21 10:51:46 +0200

    feat: Opening share sheet with bottom sheet

--- SwissTransferFeatures/UploadProgressView/UploadSuccess/UploadSuccessQRCodeView.swift
+++ SwissTransferFeatures/UploadProgressView/UploadSuccess/UploadSuccessQRCodeView.swift
@@ -26,6 +26,20 @@ import SwiftUI
 import SwissTransferCore
 import SwissTransferCoreUI
 
+struct ActivityViewController: UIViewControllerRepresentable {
+    var activityItems: [Any]
+    var applicationActivities: [UIActivity]? = nil
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ActivityViewController>) -> UIActivityViewController {
+        return UIActivityViewController(activityItems: activityItems, applicationActivities: applicationActivities)
+    }
+
+    func updateUIViewController(
+        _ uiViewController: UIActivityViewController,
+        context: UIViewControllerRepresentableContext<ActivityViewController>
+    ) {}
+}
+
 struct UploadSuccessQRCodeView: View {
     private static let qrCodeSize: CGFloat = 160
 
@@ -34,6 +48,7 @@ struct UploadSuccessQRCodeView: View {
     @Environment(\.dismiss) private var dismiss
 
     @State private var isShowingShareTipSheet = false
+    @State private var isSharing = false
 
     let type: TransferType
     let transferUUID: String
@@ -114,7 +129,11 @@ struct UploadSuccessQRCodeView: View {
             }
         }
         .stDiscoveryPresenter(isPresented: $isShowingShareTipSheet, bottomPadding: 0) {
-            ScreenshotQrBottomSheetView()
+            ScreenshotQrBottomSheetView(isSharing: $isSharing)
+        }
+        .sheet(isPresented: $isSharing) {
+            ActivityViewController(activityItems: [transferURL!])
+                .presentationDetents([.medium])
         }
     }
 }
