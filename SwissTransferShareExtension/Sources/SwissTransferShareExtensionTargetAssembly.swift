53af28d3d863306808665b8d4c50aada191421c0
Valentin Perignon <valentin.perignon@infomaniak.com>
2026-05-26 13:00:00 +0200

    fix: SwissTransferShareExtensionTargetAssembly should inherit SwissTransferTargetAssembly

--- SwissTransferShareExtension/Sources/SwissTransferShareExtensionTargetAssembly.swift
+++ SwissTransferShareExtension/Sources/SwissTransferShareExtensionTargetAssembly.swift
@@ -16,79 +16,20 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import InfomaniakCore
+import InfomaniakCoreUIKit
 import InfomaniakDI
-import STRootTransferView
-import SwiftUI
 import SwissTransferCore
-import SwissTransferCoreUI
-import UIKit
 
-class ShareViewController: UIViewController {
-    // periphery:ignore - Making sure the Sentry is initialized at a very early stage of the extension launch.
-    private let sentryService = SentryService()
-    // periphery:ignore - Making sure the DI is registered at a very early stage of the extension launch.
-    private let dependencyInjectionHook = SwissTransferShareExtensionTargetAssembly()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Modify sheet size on iPadOS, property is ignored on iOS
-        preferredContentSize = CGSize(width: 540, height: 620)
-
-        guard let extensionItems: [NSExtensionItem] = extensionContext?.inputItems.compactMap({ $0 as? NSExtensionItem }),
-              !extensionItems.isEmpty else {
-            dismiss(animated: true)
-            return
-        }
-
-        let itemProviders: [NSItemProvider] = extensionItems.filteredItemProviders
-        guard !itemProviders.isEmpty else {
-            dismiss(animated: true)
-            return
-        }
-
-        let importedItems = itemProviders.map { ImportedItem(item: $0) }
-
-        Task {
-            @InjectService var accountManager: AccountManager
-            guard let userSession = await accountManager.getCurrentUserSession() else {
-                dismiss(animated: true)
-                return
+class SwissTransferShareExtensionTargetAssembly: SwissTransferTargetAssembly {
+    override class func getTargetServices() -> [Factory] {
+        return [
+            Factory(type: AlertPresentable.self) { _, _ in
+                AlertPresenter()
+            },
+            Factory(type: ApplicationStatable.self) { _, _ in
+                ApplicationState()
             }
-
-            let mainViewState = MainViewState(
-                swissTransferManager: userSession.swissTransferManager,
-                uploadBackendRouter: UploadBackendRouter(
-                    currentUser: userSession.userProfile,
-                    swissTransferManager: userSession.swissTransferManager
-                )
-            )
-
-            let rootTransferView = RootTransferView(initialItems: importedItems)
-                .tint(.ST.primary)
-                .ikButtonTheme(.swissTransfer)
-                .detectCompactWindow()
-                .environment(\.shareExtensionContext, ShareExtensionContext { self.dismiss(animated: true) })
-                .environment(\.currentUser, userSession.userProfile)
-                .environmentObject(mainViewState)
-                .defaultAppStorage(.shared)
-
-            let hostingController = UIHostingController(rootView: rootTransferView)
-            hostingController.view.translatesAutoresizingMaskIntoConstraints = false
-            addChild(hostingController)
-            view.addSubview(hostingController.view)
-            hostingController.didMove(toParent: self)
-
-            NSLayoutConstraint.activate([
-                hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-                hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-                hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
-                hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-            ])
-        }
-    }
-
-    override func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
-        extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
+        ]
     }
 }
