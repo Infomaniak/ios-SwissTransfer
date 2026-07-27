6c5a08c68e16f818a80355024a7529dd9abdbec9
Killian Mathias <killian.mathias@infomaniak.com>
2026-07-27 10:27:17 +0200

    feat: Add organization image

---
+++ SwissTransferCoreUI/Components/Organization/OrganizationImage.swift
@@ -0,0 +1,45 @@
+/*
+ Infomaniak SwissTransfer - iOS App
+ Copyright (C) 2026 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import DesignSystem
+import SwiftUI
+
+struct OrganizationImage: View {
+    let backgroundColor: Color
+    let image: Image
+    let size: CGFloat
+
+    init(backgroundColor: Color = .white, image: Image, size: CGFloat) {
+        self.backgroundColor = backgroundColor
+        self.image = image
+        self.size = size
+    }
+
+    var body: some View {
+        ZStack {
+            backgroundColor
+
+            image
+                .resizable()
+                .scaledToFill()
+        }
+        .frame(width: size, height: size)
+        .clipShape(RoundedRectangle(cornerRadius: IKRadius.small))
+        .drawingGroup()
+    }
+}
